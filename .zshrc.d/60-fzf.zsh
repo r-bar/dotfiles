ebabc0e1ca80501d86b208932f1a9996fb1e89d6
Ryan Barth <ryan@barth.tech>
2021-08-21 16:29:20 -0700

    make fzf loading more flexible

--- .zshrc.d/60-fzf.zsh
+++ .zshrc.d/60-fzf.zsh
@@ -1,16 +1,33 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  #export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+if ! hash fzf 2> /dev/null && [[ -d $HOME/.fzf/bin ]] && [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+COMPLETION=(
+  $HOME/.fzf/shell/completion.zsh
+  /usr/share/fzf/completion.zsh
+)
+for file in $COMPLETION[@]; do
+  if [[ $- == *i* ]] && [[ -f $file ]]; then
+    echo found completion
+    source $file
+    break
+  fi
+done
 
 # Key bindings
 # ------------
-[ -f "$HOME/.fzf/shell/key-bindings.zsh" ] && source "$HOME/.fzf/shell/key-bindings.zsh"
-
-# fzf functions for git
-#autoload fbr fco fco_preview
+KEY_BINDINGS=(
+  "$HOME/.fzf/shell/key-bindings.zsh"
+  /usr/share/fzf/key-bindings.zsh
+)
+for file in $KEY_BINDINGS[@]; do
+  if [ -f $file ]; then
+    echo fount keys
+    source $file
+    break
+  fi
+done

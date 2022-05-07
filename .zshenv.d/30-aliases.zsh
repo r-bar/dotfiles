12acbd1f4a6a766abebdde1111a542206ecb7a58
Ryan Barth <ryan@barth.tech>
2022-05-06 23:36:15 -0700

    add flyer function wrapper and update flyrc

--- .zshenv.d/30-aliases.zsh
+++ .zshenv.d/30-aliases.zsh
@@ -2,3 +2,5 @@
 alias gconfig='gcloud config configurations'
 alias gr='cd ./$(git rev-parse --show-cdup)'
 alias strip-escape='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"'
+
+hash perl-rename 2> /dev/null && alias prename='perl-rename'

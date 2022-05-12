303751292cd24a498152499f9d2e8790bc12fe51
Ryan Barth <ryan@barth.tech>
2022-05-12 12:01:43 -0700

    unify kubeconfig settings for zsh

--- .zshenv.d/20-kubernetes.zsh
+++ .zshenv.d/20-kubernetes.zsh
@@ -2,13 +2,24 @@
 ### Kubernetes ###
 ##################
 
-if [ -d "${KREW_ROOT:-$HOME/.krew}" ]; then export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"; fi
-if [ -n "$HOME/.kube/config.d" ]; then
+function kubectl-reload {
   export KUBECONFIG="$HOME/.kube/config"
   for config in $HOME/.kube/config.d/*.yaml; do
     export KUBECONFIG="${KUBECONFIG}:$config"
   done
-fi
+  if [ -d "$HOME/.k3d" ]; then
+    for config in $HOME/.k3d/kubeconfig-*.yaml; do
+      if [ -z "$KUBECONFIG" ]; then
+        export KUBECONFIG="$config"
+      else
+        export KUBECONFIG="${KUBECONFIG}:$config"
+      fi
+    done
+  fi
+}
+
+if [ -d "${KREW_ROOT:-$HOME/.krew}" ]; then export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"; fi
+kubectl-reload
 
 alias k='kubectl'
 alias kc='kubectl --context'
--- .zshrc.d/60-kubectl-reload.zsh
+++
@@ -1,6 +0,0 @@
-function kubectl-reload {
-  export KUBECONFIG="$HOME/.kube/config"
-  for config in $HOME/.kube/config.d/*.yaml; do
-    export KUBECONFIG="${KUBECONFIG}:$config"
-  done
-}

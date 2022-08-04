9cb5c95a11da9463b470be8ae16232cd1664fd45
Ryan Barth <rbarth@endpointclosing.com>
2022-08-04 12:00:27 -0700

    neovim: re-enable vim-polyglot for specific languages

--- .config/nvim/lua/syntax.lua
+++ .config/nvim/lua/syntax.lua
@@ -4,11 +4,11 @@
 local M = {}
 
 M.packages = {
-  --Package:new{'https://github.com/sheerun/vim-polyglot.git'},
   Package:new{'https://github.com/coddingtonbear/confluencewiki.vim'},
   Package:new{'https://github.com/towolf/vim-helm.git'},
   Package:new{'https://github.com/google/vim-jsonnet.git'},
   Package:new{'https://github.com/r-bar/ebnf.vim.git'},
+  Package:new{'https://github.com/sheerun/vim-polyglot.git', ['for'] = {'plantuml'}},
 }
 
 return M

2a9d8e8d2ebe3653954426462874e557cca6ba2c
Ryan Barth <rbarth@endpointclosing.com>
2022-10-18 12:34:48 -0700

    neovim: add tokyo night theme

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -2,7 +2,7 @@ local M = {}
 
 
 local LOAD_ALL = false
-local colorscheme = 'nordfox'
+local colorscheme = 'tokyonight-night'
 
 
 local function colormatch(pattern)
@@ -172,11 +172,13 @@ M.packages = {
   },
   Package:new{
     'https://github.com/rose-pine/neovim.git',
+    enabled = colormatch 'rose-pine',
     as = 'rose-pine',
     tag = 'v1.*',
   },
   Package:new{
     'https://github.com/EdenEast/nightfox.nvim.git',
+    enabled = true,
     config = function()
       -- There is no need to call setup if you don't want to change the default options and settings.
       require('nightfox').setup({
@@ -213,6 +215,47 @@ M.packages = {
           dim_inactive = true,
           terminal_colors = true,
         },
+      })
+    end,
+  },
+  Package:new{
+    'https://github.com/folke/tokyonight.nvim.git',
+    enabled = true,
+    config = function()
+      require("tokyonight").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        light_style = "day", -- The theme is used when the background is set to light
+        transparent = false, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark", -- style for floating windows
+        },
+        sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+        dim_inactive = false, -- dims inactive windows
+        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        --on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        --on_highlights = function(highlights, colors) end,
       })
     end,
   },

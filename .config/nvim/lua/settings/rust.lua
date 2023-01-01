526125aebdfc4d72708280ba0834c82cf8d6091d
Ryan Barth <ryan@barth.tech>
2022-12-31 23:04:16 -0800

    docker config updates

--- .config/nvim/lua/settings/rust.lua
+++ .config/nvim/lua/settings/rust.lua
@@ -5,32 +5,32 @@ M.packages = {
 }
 
 function M.config()
-  vim.g.tagbar_type_rust = {
-    ctagstype = "rust",
-    kinds = {
-      "T:types,type definitions",
-      "f:functions,function definitions",
-      "g:enum,enumeration names",
-      "s:structure names",
-      "m:modules,module names",
-      "c:consts,static constants",
-      "t:traits,traits",
-      "i:impls,trait implementations",
-    }
-  }
+  --vim.g.tagbar_type_rust = {
+  --  ctagstype = "rust",
+  --  kinds = {
+  --    "T:types,type definitions",
+  --    "f:functions,function definitions",
+  --    "g:enum,enumeration names",
+  --    "s:structure names",
+  --    "m:modules,module names",
+  --    "c:consts,static constants",
+  --    "t:traits,traits",
+  --    "i:impls,trait implementations",
+  --  }
+  --}
   vim.g.rustfmt_autosave = 1
 
-  require("lsp-zero").use("rust_analyzer", {
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = { loadOutDirsFromCheck = true };
-        procMacro = { enable = true };
-        updates = { channel = "nightly" };
-        checkOnSave = { command = "clippy" };
-        diagnostics = { experimental = { enable = true } };
-      };
-    };
-  })
+  --require("lsp-zero").use("rust_analyzer", {
+  --  settings = {
+  --    ["rust-analyzer"] = {
+  --      cargo = { loadOutDirsFromCheck = true };
+  --      procMacro = { enable = true };
+  --      updates = { channel = "nightly" };
+  --      checkOnSave = { command = "clippy" };
+  --      diagnostics = { experimental = { enable = true } };
+  --    };
+  --  };
+  --})
 end
 
 return M

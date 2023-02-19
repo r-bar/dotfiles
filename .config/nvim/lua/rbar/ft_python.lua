41ea033b00046aeec72d35b9d6c221cfa70901ee
Ryan Barth <ryan@barth.tech>
2023-02-19 01:20:24 -0800

    nvim: update packages

--- .config/nvim/lua/rbar/ft_python.lua
+++ .config/nvim/lua/rbar/ft_python.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.packages(use)
-  use 'https://github.com/Vimjas/vim-python-pep8-indent.git'
+  use {'https://github.com/Vimjas/vim-python-pep8-indent.git', ft = "python"}
   use 'Glench/Vim-Jinja2-Syntax'
 end
 

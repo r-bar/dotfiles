8156f5515277e1769c6ae0f495e73193f3f6e8ae
Ryan Barth <ryan@barth.tech>
2023-02-24 12:09:10 -0800

    nix: configure python packages

---
+++ .config/nixpkgs/python-packages.nix
@@ -0,0 +1,23 @@
+{ pkgs }:
+
+pkgs.python311.withPackages (p: with p; [
+  httpx # async HTTP
+  beautifulsoup4 # web scraping
+  folium # maps
+  geopy # geographical data
+  ipython # interactive shell
+  matplotlib # plots
+  networkx # graphs
+  numpy # numerical computation
+  pandas # data analysis
+  requests # HTTP library
+  scapy
+  fastapi
+  uvicorn
+  aiofiles
+  sqlalchemy
+  asyncpg
+  pip
+  python-lsp-server
+  black
+])

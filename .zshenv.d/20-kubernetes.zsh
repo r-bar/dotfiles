e8443c4717be3b2fdd35d6e1adc9b83aab06daa8
Ryan Barth <ryan@barth.tech>
2023-03-30 13:29:03 -0700

    kubectl: add kubectl-ctx stop using kubectx and modifying KUBECONFIG

--- .zshenv.d/20-kubernetes.zsh
+++ .zshenv.d/20-kubernetes.zsh
@@ -2,28 +2,28 @@
 ### Kubernetes ###
 ##################
 
-function kubectl-reload {
-  export KUBECONFIG="$HOME/.kube/config"
-  for config in $HOME/.kube/config.d/*.yaml; do
-    export KUBECONFIG="${KUBECONFIG}:$config"
-  done
-  if [ -d "$HOME/.k3d" ]; then
-    for config in $HOME/.k3d/kubeconfig-*.yaml; do
-      if [ -z "$KUBECONFIG" ]; then
-        export KUBECONFIG="$config"
-      else
-        export KUBECONFIG="${KUBECONFIG}:$config"
-      fi
-    done
-  fi
-}
+#function kubectl-reload {
+#  export KUBECONFIG="$HOME/.kube/config"
+#  for config in $HOME/.kube/config.d/*.yaml; do
+#    export KUBECONFIG="${KUBECONFIG}:$config"
+#  done
+#  if [ -d "$HOME/.k3d" ]; then
+#    for config in $HOME/.k3d/kubeconfig-*.yaml; do
+#      if [ -z "$KUBECONFIG" ]; then
+#        export KUBECONFIG="$config"
+#      else
+#        export KUBECONFIG="${KUBECONFIG}:$config"
+#      fi
+#    done
+#  fi
+#}
 
-if [ -d "${KREW_ROOT:-$HOME/.krew}" ]; then export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"; fi
-kubectl-reload
+#if [ -d "${KREW_ROOT:-$HOME/.krew}" ]; then export PATH="$PATH:${KREW_ROOT:-$HOME/.krew}/bin"; fi
+#kubectl-reload
 
 alias k='kubectl'
 alias kc='kubectl --context'
 alias kall='kubectl api-resources --verbs=list --namespaced -o name \
   | xargs -n 1 kubectl get --show-kind --ignore-not-found'
-alias kcd='kubectx'
-alias kns='kubens'
+#alias kcd='kubectx'
+#alias kns='kubens'

65fd95f1d8c4440157ec1f6b48be25b632312938
Ryan Barth <ryan@barth.tech>
2024-06-16 16:17:46 -0700

    zigup: fix path link directory

--- .zshrc.d/40-zigup.zsh
+++ .zshrc.d/40-zigup.zsh
@@ -2,4 +2,4 @@ if [[ ! -d ~/.local/share/zigup ]]; then
   mkdir -p ~/.local/share/zigup
 fi
 
-alias zigup='zigup --install-dir ~/.local/share/zigup --path-link /usr/local/bin/zig'
+alias zigup='zigup --install-dir ~/.local/share/zigup --path-link $HOME/.local/bin/zig'

dbf4bb7c27bedb244253aa589263fb7d61a2c688
Ryan Barth <ryan@barth.tech>
2024-07-25 03:04:05 -0700

    mise: fix completions

--- .zshrc.d/60-mise.zsh
+++ .zshrc.d/60-mise.zsh
@@ -1,4 +1,12 @@
 if hash mise 2>/dev/null; then
   eval "$(mise activate $(basename $SHELL))"
   alias mr="mise run"
+  alias m="mise"
+  alias mt="mise tasks"
+
+  compfile=~/.local/share/zinit/completions/_mise
+  if [ ! -f $compfile ]; then
+    mkdir -p $(dirname $compfile)
+    mise completion zsh > $compfile
+  fi
 fi

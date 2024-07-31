f3211280a4b1f6ffcb6ee7088a6f2271e9f388d5
Ryan Barth <ryan@barth.tech>
2024-07-31 00:00:48 -0700

    hammerspoon: add global mute hotkeys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,4 @@
+-- Activity Manager --
 awakeTimer = nil
 
 function sleep(n)
@@ -30,3 +31,23 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "A", function()
 
   awakeTimer = hs.timer.doUntil(function() return awakeTimer == nil end, activity, 5)
 end)
+
+-- Global Mute Hotkeys --
+local hyper     = {"ctrl", "alt", "cmd"}
+local lesshyper = {"ctrl", "alt"}
+hs.loadSpoon("GlobalMute")
+spoon.GlobalMute:bindHotkeys({
+  unmute = {lesshyper, "u"},
+  mute   = {lesshyper, "m"},
+  toggle = {hyper, "space"}
+})
+spoon.GlobalMute:configure({
+  unmute_background = 'file:///Library/Desktop%20Pictures/Solid%20Colors/Red%20Orange.png',
+  mute_background   = 'file:///Library/Desktop%20Pictures/Solid%20Colors/Turquoise%20Green.png',
+  enforce_desired_state = true,
+  stop_sococo_for_zoom  = true,
+  unmute_title = "<---- THEY CAN HEAR YOU -----",
+  mute_title = "<-- MUTE",
+  -- change_screens = "SCREENNAME1, SCREENNAME2"  -- This will only change the background of the specific screens.  string.find()
+})
+spoon.GlobalMute._logger.level = 3

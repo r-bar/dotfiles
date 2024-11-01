956ea8c67f95ec3cea3b7a5267dd15d17b1a0706
Ryan Barth <ryan@barth.tech>
2024-11-01 00:51:16 -0700

    hammerspoon: make the mic mute description smaller

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -47,8 +47,8 @@ spoon.GlobalMute:configure({
   mute_background   = 'file:///Library/Desktop%20Pictures/Solid%20Colors/Turquoise%20Green.png',
   enforce_desired_state = true,
   stop_sococo_for_zoom  = true,
-  unmute_title = "<---- THEY CAN HEAR YOU -----",
-  mute_title = "<-- MUTE",
+  unmute_title = "",
+  mute_title = "",
   -- change_screens = "SCREENNAME1, SCREENNAME2"  -- This will only change the background of the specific screens.  string.find()
 })
 spoon.GlobalMute._logger.level = 3

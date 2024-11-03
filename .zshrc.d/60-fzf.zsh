66572c93449cac8fec4ebbff745a05cd8fa8afb2
Ryan Barth <ryan@barth.tech>
2024-11-02 18:35:41 -0700

    gpg: fix nixos pinentry

--- .zshrc.d/60-fzf.zsh
+++ .zshrc.d/60-fzf.zsh
@@ -1,35 +1,3 @@
-# Setup fzf
-# ---------
-if ! hash fzf 2> /dev/null && [[ -d $HOME/.fzf/bin ]] && [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+if hash fzf 2> /dev/null; then
+  source <(fzf --zsh)
 fi
-
-# Auto-completion
-# ---------------
-COMPLETION=(
-  $HOME/.fzf/shell/completion.zsh
-  /usr/share/fzf/completion.zsh
-  /opt/homebrew/opt/fzf/shell/completion.zsh
-)
-for file in $COMPLETION[@]; do
-  if [[ $- == *i* ]] && [[ -f $file ]]; then
-    #echo found completion
-    source $file
-    break
-  fi
-done
-
-# Key bindings
-# ------------
-KEY_BINDINGS=(
-  "$HOME/.fzf/shell/key-bindings.zsh"
-  /usr/share/fzf/key-bindings.zsh
-  /opt/homebrew/opt/fzf/shell/key-bindings.zsh
-)
-for file in $KEY_BINDINGS[@]; do
-  if [ -f $file ]; then
-    #echo found keys
-    source $file
-    break
-  fi
-done

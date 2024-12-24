cf6d24e73eeaad60a5d6939c3cfa6872cb4be170
Ryan Barth <ryan@barth.tech>
2024-12-24 00:14:59 -0800

    nvim: remove duplicate matchit plugin

--- .config/nvim/lua/rbar/base.lua
+++ .config/nvim/lua/rbar/base.lua
@@ -50,7 +50,6 @@ function M.packages(use)
   }
   use 'https://github.com/wellle/targets.vim'
   use {'danro/rename.vim', cmd = "Rename"}
-  use 'https://github.com/tmhedberg/matchit.git'
   use { 'https://github.com/Valloric/MatchTagAlways.git', enabled = false }
   use { 'https://github.com/andymass/vim-matchup' }
   use 'https://github.com/kana/vim-textobj-user.git'

a2e72d16934d671c82d389cfec03510bc278b1cd
Ryan Barth <ryan@barth.tech>
2025-01-04 22:57:47 -0800

    fish: fix bad mise command not found behavior

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
+if test -d ~/.cargo/bin
+  set fish_user_paths ~/.cargo/bin $fish_user_paths
+end
+set fish_user_paths ~/.local/bin $fish_user_paths
+
 if status is-interactive
   config_activate_mise
 end

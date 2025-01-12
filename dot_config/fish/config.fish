84b1a14f047b623983275418c7b3a94f55eb0b21
Ryan Barth <ryan@barth.tech>
2025-01-11 21:59:50 -0800

    fish: set upload tool env variables

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,9 @@
 set -gx EDITOR nvim
 
+set -gx UPLOAD_RCLONE_CONFIG linode
+set -gx UPLOAD_BUCKET barth-tech
+set -gx UPLOAD_BASE_PATH uploads
+
 if test -d ~/.cargo/bin
   set fish_user_paths ~/.cargo/bin $fish_user_paths
 end

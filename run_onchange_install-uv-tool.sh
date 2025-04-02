adae9863645dde6d3e8634515fc52ec37911815f
Ryan Barth <ryan@barth.tech>
2025-04-01 22:15:56 -0700

    chezmoi: add uv tool install script

---
+++ run_onchange_install-uv-tool.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+uv tool install black
+uv tool install httpie
+uv tool install ipython
+uv tool install keyring
+uv tool install poetry
+uv tool install pre-commit
+uv tool install python-lsp-server --with pylsp-rope --with pylsp-mypy
+uv tool install ruff
+uv tool install mypy

c0bf0d2195432ea7e6eacfbaeb6f7732e669f7a2
Ryan Barth <ryan@barth.tech>
2025-05-14 09:51:57 -0700

    ensure python packages are updated

--- run_onchange_install-uv-tool.sh
+++ run_onchange_install-uv-tool.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-uv tool install black
-uv tool install httpie
-uv tool install ipython
-uv tool install keyring
-uv tool install poetry
-uv tool install pre-commit
-uv tool install python-lsp-server --with pylsp-rope --with pylsp-mypy
-uv tool install ruff
-uv tool install mypy
+uv tool install black --upgrade
+uv tool install httpie --upgrade
+uv tool install ipython --upgrade
+uv tool install poetry --upgrade --with keyring
+uv tool install pre-commit --upgrade
+uv tool install python-lsp-server --upgrade --with pylsp-rope --with pylsp-mypy
+uv tool install ruff --upgrade
+uv tool install mypy --upgrade

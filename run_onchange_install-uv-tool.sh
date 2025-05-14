ff50031723e1d683ed2ea7231251f7576b1dec10
Ryan Barth <ryan@barth.tech>
2025-05-14 12:38:52 -0700

    add google artifact registry keyring

--- run_onchange_install-uv-tool.sh
+++ run_onchange_install-uv-tool.sh
@@ -3,8 +3,9 @@
 uv tool install black --upgrade
 uv tool install httpie --upgrade
 uv tool install ipython --upgrade
-uv tool install poetry --upgrade --with keyring
+# GAR keyring plugin required for appomni poetry setup
+uv tool install poetry --upgrade --with keyring --with keyrings.google-artifactregistry-auth
 uv tool install pre-commit --upgrade
-uv tool install python-lsp-server --upgrade --with pylsp-rope --with pylsp-mypy
+uv tool install python-lsp-server --upgrade --with pylsp-rope --with pylsp-mypy --with keyrings.google-artifactregistry-auth
 uv tool install ruff --upgrade
 uv tool install mypy --upgrade

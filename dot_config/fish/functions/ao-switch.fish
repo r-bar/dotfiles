e56b28cb1dd56e319a3809e1fdf1d31fe4264fb6
Ryan Barth <ryan@barth.tech>
2025-07-08 14:16:20 -0700

    ao-switch: use tmux session variables to export the gcloud context

--- dot_config/fish/functions/ao-switch.fish
+++ dot_config/fish/functions/ao-switch.fish
@@ -1,72 +1,84 @@
 function ao-switch --description 'Switch the AppOmni gcloud and k8s environment'
+  argparse 'g/global'
   set -l context $argv[1]
+
+  function _set
+    set -gx $argv[1] $argv[2]
+    if set -ql _flag_global && test -n "$TMUX"
+      echo Set globally
+      tmux setenv $argv[1] $argv[2]
+    end
+  end
+
   switch $context
+
   case int int-us1
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-int-us1;
-    set -gx PROJECT appomni-sfdc-demo;
-    set -gx NAMESPACE dev;
-    set -gx AOPOD "int-us1"
-    gcloud config set compute/zone us-central1-c;
-    gcloud config set project appomni-sfdc-demo;
-    gcloud container clusters get-credentials cluster-vpc1;
-    kubectl config set-context --current --namespace=$NAMESPACE;
-  case qa1
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-qa-us1;
-    set -gx PROJECT appomni-qa-us1;
-    set -gx NAMESPACE qa;
-    set -gx AOPOD "qa-us1"
-    gcloud config set compute/zone us-central1-c;
-    gcloud config set project appomni-qa-us1;
-    gcloud container clusters get-credentials appomni-qa-us1 --zone=us-central1;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-int-us1
+    _set PROJECT appomni-sfdc-demo
+    _set NAMESPACE dev
+    _set AOPOD "int-us1"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials cluster-vpc1
+    kubectl config set-context --current --namespace=$NAMESPACE
+
+  case qa-us1
+    _set KUBECONFIG /tmp/kubeconfig-appomni-qa-us1
+    _set PROJECT appomni-qa-us1
+    _set NAMESPACE qa
+    _set AOPOD "qa-us1"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-qa-us1 --zone=us-central1
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case us1 prod prod-us1
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-prod-us1;
-    set -gx PROJECT appomni-prod-us
-    set -gx NAMESPACE prod
-    set -gx AOPOD "prod-us"
-    gcloud config set compute/zone us-central1-c;
-    gcloud config set project appomni-prod-us;
-    gcloud container clusters get-credentials appomni-prod-vpc;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-prod-us1
+    _set PROJECT appomni-prod-us
+    _set NAMESPACE prod
+    _set AOPOD "prod-us"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-prod-vpc
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case us2 prod-us2
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-prod-us2;
-    set -gx PROJECT appomni-prod-us2
-    set -gx NAMESPACE "prod"
-    set -gx AOPOD "prod-us2"
-    gcloud config set compute/zone us-west1-c;
-    gcloud config set project appomni-prod-us2;
-    gcloud container clusters get-credentials appomni-us2 --zone=us-west1;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-prod-us2
+    _set PROJECT appomni-prod-us2
+    _set NAMESPACE "prod"
+    _set AOPOD "prod-us2"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-us2 --zone=us-west1
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case us3 prod-us3
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-prod-us3;
-    set -gx PROJECT appomni-prod-us3
-    set -gx NAMESPACE prod
-    set -gx AOPOD "prod-us3"
-    gcloud config set compute/zone us-central1-f;
-    gcloud config set project appomni-prod-us3;
-    gcloud container clusters get-credentials appomni-us3 --region us-central1;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-prod-us3
+    _set PROJECT appomni-prod-us3
+    _set NAMESPACE prod
+    _set AOPOD "prod-us3"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-us3 --region us-central1
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case eu1 prod-eu1
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-prod-eu1;
-    set -gx PROJECT appomni-prod-eu1
-    set -gx NAMESPACE "prod"
-    set -gx AOPOD "prod-eu1"
-    gcloud config set compute/zone europe-west3-a;
-    gcloud config set project appomni-prod-eu;
-    gcloud container clusters get-credentials appomni-eu1 --zone=europe-west3;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-prod-eu1
+    _set PROJECT appomni-prod-eu1
+    _set NAMESPACE "prod"
+    _set AOPOD "prod-eu1"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-eu1 --zone=europe-west3
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case aus1 prod-aus1
-    set -gx KUBECONFIG /tmp/kubeconfig-appomni-prod-aus1;
-    set -gx PROJECT appomni-prod-us3
-    set -gx NAMESPACE "prod"
-    set -gx AOPOD "prod-aus1"
-    gcloud config set compute/zone australia-southeast1-a;
-    gcloud config set project appomni-prod-aus1;
-    gcloud container clusters get-credentials appomni-aus1 --zone=australia-southeast1;
-    kubectl config set-context --current --namespace=$NAMESPACE;
+    _set KUBECONFIG /tmp/kubeconfig-appomni-prod-aus1
+    _set PROJECT appomni-prod-us3
+    _set NAMESPACE "prod"
+    _set AOPOD "prod-aus1"
+    _set CLOUDSDK_ACTIVE_CONFIG_NAME $PROJECT
+    gcloud container clusters get-credentials appomni-aus1 --zone=australia-southeast1
+    kubectl config set-context --current --namespace=$NAMESPACE
+
   case '*'
     echo "Unknown context: $context"
     return 1
+
   end
   return 0
 end

1d2653da84da6ca08cd0665aa84a08c80b58e0c3
Ryan Barth <ryan@barth.tech>
2025-08-24 13:05:37 -0700

    nvim: pest grammar

--- dot_config/nvim/exact_lua/exact_rbar/treesitter.lua
+++ dot_config/nvim/exact_lua/exact_rbar/treesitter.lua
@@ -8,7 +8,19 @@ function M.packages(use)
     run = ":TSUpdate",
     build = ":TSUpdate",
     config = function()
-      require"nvim-treesitter.configs".setup {
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.pest = {
+        install_info = {
+          url = "https://github.com/pest-parser/tree-sitter-pest.git", -- local path or git repo
+          files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+          -- optional entries:
+          branch = "main", -- default branch in case of git repo if different from master
+          generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+        },
+        filetype = "pest", -- if filetype does not match the parser name
+      }
+      require("nvim-treesitter.configs").setup {
         -- A list of parser names, or "all"
         ensure_installed = "all",
         modules = {},
@@ -63,6 +75,9 @@ function M.packages(use)
         textobjects = { enable = true },
       }
     end,
+    dependencies = {
+      "https://github.com/pest-parser/tree-sitter-pest.git",
+    },
   }
   use {
     "nvim-treesitter/playground",
--- dot_config/nvim/filetype.lua
+++ dot_config/nvim/filetype.lua
@@ -1,5 +1,6 @@
 vim.filetype.add{
   extension = {
+    pest = 'pest',
     v = 'vlang',
   },
   filename = {

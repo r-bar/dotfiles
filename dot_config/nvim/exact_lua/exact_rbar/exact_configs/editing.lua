70976c937dda35d82aafbf6bc94ba12b7fc61103
Ryan Barth <ryan@barth.tech>
2026-02-06 23:16:15 -0800

    use comment.nvim

--- dot_config/nvim/exact_lua/exact_rbar/exact_configs/editing.lua
+++ dot_config/nvim/exact_lua/exact_rbar/exact_configs/editing.lua
@@ -57,6 +57,7 @@ function M.packages(use)
 
 	use({
 		"https://github.com/scrooloose/nerdcommenter.git",
+		enabled = false,
 		keys = {
 			{ "<leader>cc", "<Plug>NERDCommenterComment", mode = { "n", "v" }, noremap = true },
 			{ "<leader>cc", "<Plug>NERDCommenterComment", mode = { "n", "v" }, noremap = true },
@@ -89,6 +90,53 @@ function M.packages(use)
 				gleam = { left = "// ", leftAlt = "/// " },
 			}
 		end,
+	})
+
+	use({
+		"numToStr/Comment.nvim",
+		opts = {
+			---Add a space b/w comment and the line
+			padding = true,
+			---Whether the cursor should stay at its position
+			sticky = true,
+			---Lines to be ignored while (un)comment
+			ignore = nil,
+			---LHS of toggle mappings in NORMAL mode
+			toggler = {
+				---Line-comment toggle keymap
+				line = "gcc",
+				---Block-comment toggle keymap
+				block = "gbc",
+			},
+			---LHS of operator-pending mappings in NORMAL and VISUAL mode
+			opleader = {
+				---Line-comment keymap
+				line = "gc",
+				---Block-comment keymap
+				block = "gb",
+			},
+			---LHS of extra mappings
+			extra = {
+				---Add comment on the line above
+				--above = 'gcO',
+				---Add comment on the line below
+				--below = 'gco',
+				---Add comment at the end of line
+				--eol = 'gcA',
+			},
+			---Enable keybindings
+			---NOTE: If given `false` then the plugin won't create any mappings
+			mappings = {
+				---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+				basic = true,
+				---Extra mapping; `gco`, `gcO`, `gcA`
+				extra = false,
+			},
+			---Function to call before (un)comment
+			pre_hook = nil,
+			---Function to call after (un)comment
+			post_hook = nil,
+		},
 	})
 
 	use({

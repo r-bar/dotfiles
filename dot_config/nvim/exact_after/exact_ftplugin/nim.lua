3bf6f83506b72e4759c055fe95293deae47c24ba
Ryan Barth <ryan@barth.tech>
2026-02-26 13:21:32 -0800

    nvim: fix nim comments

---
+++ dot_config/nvim/exact_after/exact_ftplugin/nim.lua
@@ -0,0 +1,5 @@
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+-- builtin comments=exO:]#,fs1:#[,mb:*,ex:]#,:#
+vim.o.comments = "exO:]#,fs1:#[,mb:*,ex:]#,:##,:#"
--- dot_config/nvim/exact_ftplugin/nim.lua
+++
@@ -1,4 +0,0 @@
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.comments = "s1:##,mb:##,ex:##,s1:#,mb:#,ex:#"

c8c8ec908155613f3b94abcbdafe0d660d5fb229
Ryan Barth <ryan@barth.tech>
2026-05-03 12:05:19 -0700

    nvim: update opencode controls

--- dot_config/nvim/exact_lua/exact_rbar/exact_configs/opencode.lua
+++ dot_config/nvim/exact_lua/exact_rbar/exact_configs/opencode.lua
@@ -18,25 +18,44 @@ function M.packages(use)
 
 			vim.keymap.set({ "n", "x" }, "<leader>oa", function()
 				opencode.ask("@this: ", { submit = true })
-			end, { desc = "Ask opencode…" })
+			end, { desc = "Opencode: Ask..." })
+
+			vim.keymap.set({ "n", "x" }, "<leader>on", function()
+				opencode.command("session.new")
+				opencode.ask("@this: ", { submit = true })
+			end, { desc = "Opencode: Ask in a new session" })
+
+			vim.keymap.set({ "n", "x" }, "<leader>ox", function()
+				opencode.command("session.select")
+			end, { desc = "Opencode: Select session" })
+
 			vim.keymap.set({ "n", "x" }, "<leader>ox", function()
 				opencode.select()
-			end, { desc = "Execute opencode action…" })
+			end, { desc = "Opencode: Execute action" })
+
 			vim.keymap.set({ "n", "t" }, "<leader>oo", function()
 				opencode.toggle()
-			end, { desc = "Toggle opencode" })
+			end, { desc = "Opencode: Toggle visibility" })
+
 			vim.keymap.set({ "n", "x" }, "go", function()
 				return opencode.operator("@this ")
-			end, { desc = "Add range to opencode", expr = true })
+			end, { desc = "Opencode: Add line range to prompt", expr = true })
+
 			vim.keymap.set("n", "goo", function()
 				return opencode.operator("@this ") .. "_"
-			end, { desc = "Add line to opencode", expr = true })
+			end, { desc = "Opencode: Add line to prompt", expr = true })
+
+			vim.keymap.set({ "n", "x" }, "<leader>od", function()
+				return opencode.command("prompt.clear")
+			end, { desc = "Opencode: Clear prompt", expr = true })
+
 			vim.keymap.set("n", "<S-C-u>", function()
 				opencode.command("session.half.page.up")
-			end, { desc = "Scroll opencode up" })
+			end, { desc = "Opencode: Scroll up" })
+
 			vim.keymap.set("n", "<S-C-d>", function()
 				opencode.command("session.half.page.down")
-			end, { desc = "Scroll opencode down" })
+			end, { desc = "Opencode: Scroll down" })
 		end,
 	})
 end

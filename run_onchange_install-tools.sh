1334f3e698779108e885127e3395c37fe608ef77
Ryan Barth <ryan@barth.tech>
2026-09-23 10:33:16 -0700

    chezmoi: cleanup tool install script. add pylsp-workspace-symbols plugin

--- run_onchange_install-tools.sh
+++ run_onchange_install-tools.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
-PREFIX="${PREFIX:-$HOME/.local/bin}"
+set -euo pipefail
 
+PREFIX="${PREFIX:-$HOME/.local/bin}"
 UV_TOOL_BIN_DIR="$PREFIX"
+PYLSP_PYTHON_VERSION=3.14
+PROFILES="python"
+
+case "$(hostname -s)" in
+  venus)
+    PROFILES="$PROFILES home"
+    ;;
+  earth)
+    PROFILES="$PROFILES home"
+    ;;
+  ceres)
+    PYLSP_PYTHON_VERSION=3.11
+    PROFILES="$PROFILES work"
+    ;;
+  *)
+    ;;
+esac
 
 function ubi_install() {
   mise exec ubi -- ubi -v -i "$PREFIX" $@
@@ -12,26 +30,38 @@ function uv_install() {
   mise exec uv -- uv tool install $@
 }
 
-uv_install httpie --upgrade
-uv_install ipython --upgrade
-uv_install poetry --upgrade
-uv_install pre-commit --upgrade
-uv_install ruff --upgrade
-uv_install mypy --upgrade
-uv_install ty --upgrade
-uv_install llm --upgrade --with llm-ollama --with llm-gemini --with llm-openrouter
-uv_install coconut[jupyter] --upgrade --with ipython
+function work_profile() {
+  return 0
+}
 
-if [ "$(uname)" != "Darwin" ]; then
+function home_profile() {
   uv_install rembg[cli] --upgrade --python 3.10 --with onnxruntime
-fi
+  uv_install coconut[jupyter] --upgrade --with ipython
+  ubi_install --project roc-lang/nightlies -e roc
+}
 
-if [ "$(hostname -s)" == "ceres" ]; then
-  uv_install python-lsp-server --upgrade --python 3.11 --with pylsp-rope --with pylsp-mypy --with keyrings.google-artifactregistry-auth
-else
-  uv_install python-lsp-server --upgrade --python 3.14 --with pylsp-rope --with pylsp-mypy --with keyrings.google-artifactregistry-auth
-fi
+function python_profile() {
+  uv_install ipython --upgrade
+  uv_install poetry --upgrade
+  uv_install pre-commit --upgrade
+  uv_install ruff --upgrade
+  uv_install mypy --upgrade
+  uv_install ty --upgrade
+  uv_install python-lsp-server --upgrade \
+    --python $PYLSP_PYTHON_VERSION \
+    --with pylsp-rope \
+    --with pylsp-mypy \
+    --with keyrings.google-artifactregistry-auth \
+    --with pylsp-workspace-symbols \
+    ;
+}
 
-ubi_install --project git-pkgs/forge --tag v0.5.1
-ubi_install --project dandavison/delta --tag 0.19.2
-ubi_install --project roc-lang/nightlies --matching-regex 2026-09-18 -e roc
+uv_install llm --upgrade --with llm-ollama --with llm-gemini --with llm-openrouter
+
+ubi_install --project git-pkgs/forge
+ubi_install --project dandavison/delta
+
+for profile in $PROFILES; do
+  echo Installing tools for $profile profile
+  "${profile}_profile"
+done

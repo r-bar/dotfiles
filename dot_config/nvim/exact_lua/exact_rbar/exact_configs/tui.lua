d4c943a65cff4aae0635664de51755ed097ca566
Ryan Barth <ryan@barth.tech>
2026-09-24 10:54:59 -0700

    nvim: update key for writing sql buffers in DBUI

--- dot_config/nvim/exact_lua/exact_rbar/exact_configs/tui.lua
+++ dot_config/nvim/exact_lua/exact_rbar/exact_configs/tui.lua
@@ -21,6 +21,15 @@ function M.packages(use)
 			"DBUIDeleteConnection",
 			"DBUIExecuteQuery",
 		},
+		keys = {
+			{
+				"<C-CR>",
+				"<cmd>write<cr>",
+				mode = { "n", "i" },
+				ft = "sql",
+				desc = "Write buffer (submits query in DBUI)",
+			},
+		},
 		config = function()
 			vim.g.db_ui_use_nerd_fonts = 1
 			vim.g.db_ui_use_nvim_notify = 1
